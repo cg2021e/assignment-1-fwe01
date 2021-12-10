23a852496913868fc512cacbafffa1096ace7711
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2021-12-10 07:55:37 +0700

    Challenge #2

--- Javascripts/Scene/EucalyptusOilBottleScene.js
+++ Javascripts/Scene/EucalyptusOilBottleScene.js
@@ -43,89 +43,11 @@ export class EucalyptusOilBottleScene extends Scene {
         this.addGeometry(this.lightsource_cube);
     }
 
-    _onMouseClick() {
-        Geometry.randomColor = !Geometry.randomColor;
-        this._initColorsBuffer();
-        this._bindColorBuffer();
-    }
 
-    _onKeyDown(event) {
-        //W
-        if (event.keyCode === 87) {
-            this.moveLightSourceUp = true
-        }
-        //S
-        if (event.keyCode === 83) {
-            this.moveLightSourceDown = true
-        }
-        //A
-        if (event.keyCode === 65) {
-            this.moveCameraLeft = true
-        }
-        //D
-        if (event.keyCode === 68) {
-            this.moveCameraRight = true
-        }
-        //Enter
-        if (event.keyCode === 13) {
-            this.rotateWorld = !this.rotateWorld;
-        }
-    }
 
     animate() {
         let startTime = new Date();
-        this._update();
         this._render();
         let endTime = new Date();
-        // let timeDiff = endTime - startTime;
-        // console.log(timeDiff);
-    }
-
-    _update() {
-        let vertexChanged = false;
-        let viewChanged = false;
-
-        if (this.moveLightSourceUp) {
-            this.lightsource_cube.translate(new Vector3(0, this.movementSpeed, 0));
-            this.lightSourcePosition[1] += this.movementSpeed
-            this.moveLightSourceUp = false
-            vertexChanged = true;
-        }
-        if (this.moveLightSourceDown) {
-            this.lightsource_cube.translate(new Vector3(0, -this.movementSpeed, 0));
-            this.lightSourcePosition[1] -= this.movementSpeed
-            this.moveLightSourceDown = false
-            vertexChanged = true;
-        }
-        if (this.moveCameraLeft) {
-            this.camera[0] -= 0.01
-            this.lookAt[0] -= 0.01
-            this.moveCameraLeft = false
-            viewChanged = true;
-        }
-        if (this.moveCameraRight) {
-            this.camera[0] += 0.01
-            this.lookAt[0] += 0.01
-            this.moveCameraRight = false
-            viewChanged = true;
-        }
-
-        if (vertexChanged) {
-            this._initVerticesBuffer();
-            this._initNormalsBuffer();
-            this._bindVertexBuffer();
-            this._bindNormalBuffer();
-        }
-
-        if (viewChanged) {
-            this._initViewMatrix();
-        }
-
-        if (this.rotateWorld) {
-            this.webGlUtils.rotateZ(this.rotationMatrix, 0.002);
-            this.webGlUtils.rotateY(this.rotationMatrix, 0.002);
-            this.webGlUtils.rotateX(this.rotationMatrix, 0.002);
-        }
-        this._bindUniforms();
     }
 }
